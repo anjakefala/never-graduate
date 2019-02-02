bdccf527925ecb0e7da30becaca2e34f73e5eff0
anjakefala <anja.kefala@gmail.com>
2019-02-02 11:48:30 -0500

    Use csv sniffer to automatically infer the delimiters in the file.

--- python-morsels/fix_csv/fix_csv.py
+++ python-morsels/fix_csv/fix_csv.py
@@ -4,13 +4,18 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('inputs', nargs=2, help='source and dest')
-parser.add_argument('--in-delimiter', dest='delimiter', default='|')
+parser.add_argument('--in-delimiter', dest='delimiter', default=None)
 parser.add_argument('--in-quote', dest='quotechar', default='"')
 args = parser.parse_args()
 
 with open(args.inputs[0], 'r') as i_fp:
     with open(args.inputs[1], 'w+') as o_fp:
-        rdr = csv.reader(i_fp, delimiter=args.delimiter, quotechar=args.quotechar)
+        if args.delimiter is not None:
+            rdr = csv.reader(i_fp, delimiter=args.delimiter, quotechar=args.quotechar)
+        else:
+            dialect = csv.Sniffer().sniff(i_fp.read())
+            i_fp.seek(0)
+            rdr = csv.reader(i_fp, dialect)
         cw = csv.writer(o_fp)
         for row in rdr:
             cw.writerow(row)
