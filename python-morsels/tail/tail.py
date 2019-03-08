84ab9637f3c7ecf3f15b0a30a0e310ff159db6fc
anjakefala <anja.kefala@gmail.com>
2019-03-08 18:59:54 -0500

    Completed the second bonus.

--- python-morsels/tail/tail.py
+++ python-morsels/tail/tail.py
@@ -1,2 +1,16 @@
 def tail(sequence, n):
-    return list(sequence[-n:]) if n > 0 else []
+
+    if n <= 0:
+        return []
+
+    tailed_sequence = []
+
+    for el in sequence:
+        if len(tailed_sequence) == n and n > 1:
+            tailed_sequence = tailed_sequence[1:] + [el]
+        elif len(tailed_sequence) == n and n == 1:
+            tailed_sequence = [el]
+        else:
+            tailed_sequence.append(el)
+
+    return tailed_sequence
