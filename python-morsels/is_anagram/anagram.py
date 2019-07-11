faffc70db851e4ee37454ad25fcd1da561b0e548
anjakefala <anja.kefala@gmail.com>
2019-07-11 13:18:00 -0700

    Revised with Trey Hunner's feedback

--- python-morsels/is_anagram/anagram.py
+++ python-morsels/is_anagram/anagram.py
@@ -1,25 +1,14 @@
 import string
 
+def letters_in(word):
+    return sorted(
+            char
+            for char in word.lower()
+            if char.isalpha()
+        )
+
 def is_anagram(a, b):
 
-    # normalise the two lists: all lowercase, no spaces and no punctuation
-    excluded = list(string.punctuation) + [' ']
-    a = [letter.lower() for letter in a if letter not in excluded]
-    b = [letter.lower() for letter in b if letter not in excluded]
+    a, b = a.lower(), b.lower()
+    return letters_in(a) == letters_in(b)
 
-    # zip is going to iterate until the shortest string runs out of length
-    # since to be a proper anagram, they need to be the same length
-    # check for length first
-    if len(a) != len(b):
-        return False
-
-    # sort after checking for length
-    # if they are wrong length, you will not pay the time cost of sorting first
-    a.sort()
-    b.sort()
-
-    for i, j in zip(a, b):
-        if i != j:
-            return False
-
-    return True
