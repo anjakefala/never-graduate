315dda8f05e41edcdee6a3c57c55ef37e1b9c5f9
anjakefala <anja.kefala@gmail.com>
2019-07-26 14:14:02 -0400

    Messier solution which takes generators into account

--- python-morsels/multimax/multimax.py
+++ python-morsels/multimax/multimax.py
@@ -1,3 +1,12 @@
 def multimax(sequence, key=max):
+    curr_max = []
+    for i in sequence:
+        if not curr_max:
+            curr_max.append(i)
+        else:
+            if i > curr_max[0]:
+                curr_max = [i]
+            elif i == curr_max[0]:
+                curr_max.append(i)
+    return curr_max
 
-    return [i for i in sequence if i == key(sequence)]
