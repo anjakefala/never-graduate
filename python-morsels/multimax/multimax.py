509eed4e9971f9e566964ff98952889a42db7d8b
anjakefala <anja.kefala@gmail.com>
2019-07-26 14:39:34 -0400

    All bonuses complete. This is gross, but I guess it works

--- python-morsels/multimax/multimax.py
+++ python-morsels/multimax/multimax.py
@@ -1,12 +1,16 @@
-def multimax(sequence, key=max):
+def same(val):
+    return val
+
+def multimax(sequence, key=same):
     curr_max = []
     for i in sequence:
         if not curr_max:
             curr_max.append(i)
         else:
-            if i > curr_max[0]:
-                curr_max = [i]
-            elif i == curr_max[0]:
-                curr_max.append(i)
+            if max(key(i), key(curr_max[0])) == key(i):
+                if key(i) != key(curr_max[0]):
+                    curr_max = [i]
+                else:
+                    curr_max.append(i)
     return curr_max
 
